24cb5089e3f149b47765b1dc4817964e67ba8b41
Gal.s <g@gal77.com>
2016-08-22 14:57:14 +0300

    5 commit

--- app.js
+++ app.js
@@ -8,22 +8,14 @@ var LocalStrategy = require('passport-local').Strategy;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var expressSession = require('express-session');
+app.use(expressSession({ secret: 'mySecretKey' }));
+
 app.use(passport.initialize());
 
-app.post('/login', passport.authenticate('login', {
-  successRedirect: '/success',
-  failureRedirect: '/login',
-  session: false
-
-
-}));
-
-  app.get('/success', function (req, res){
-  res.send("Hey, hello from the server!");
-})
-
-app.get('/login', function (req, res) {
-  res.sendFile(__dirname + '/login.html');
+passport.serializeUser(function (user, done) {
+  console.log(user);
+  done(null, user);    // Could store just the id using done(null, user.id);
 });
 
 passport.use('login', new LocalStrategy(function (username, password, done) {
@@ -35,5 +27,18 @@ passport.use('login', new LocalStrategy(function (username, password, done) {
     return done(null, false);       
   }
 }));
+
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/success',
+  failureRedirect: '/login'
+}));
+
+app.get('/success', function (req, res){
+  res.send('Hey, hello from the server!');
+})
+
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/login.html');
+});
 
 app.listen(8000);
